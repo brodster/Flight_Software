898987a869e0acf4c753769f9857358ec1bdbd93
Tayoshonibare <oshoniba@ryerson.ca>
2015-03-11 15:38:37 -0400

    Buzzer Update
    Buzzer Update

--- Buzzer/Buzzer_function/Buzzer_function.ino
+++ Buzzer/Buzzer_function/Buzzer_function.ino
@@ -1,12 +1,11 @@
+buzzer_pin = 8;
 void setup (){}
 
 void loop (){
 buzzer();
 }
 
-
-
 //Function is used at the end to make the Buzzer active
 void buzzer () {
-tone (8, 262);
+tone (buzzer_pin, 262);
 }
