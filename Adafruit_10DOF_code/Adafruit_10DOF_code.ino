0f85da49b7c0ef581769c734db0416618deb711c
Tayoshonibare <oshoniba@ryerson.ca>
2015-04-01 03:31:21 -0400

    X, Y, z ROLL RATE AND ORIENTATION ADDED
    More described adafruit orientation data in x, y and z cordinates rather
    than roll, descent and z

--- Adafruit_10DOF_code/Adafruit_10DOF_code.ino
+++ Adafruit_10DOF_code/Adafruit_10DOF_code.ino
@@ -1,95 +1,117 @@
-//THIS CODE IS FOR THE ADAFRUIT 10 DOF SENSOR------
-//CHECK BUTTOM FOR FUNCTIONS
-#include <Wire.h> 
-#include <Adafruit_Sensor.h>
-#include <Adafruit_LSM303_U.h>              
-#include <Adafruit_BMP085_U.h>
-#include <Adafruit_L3GD20_U.h>
-#include <Adafruit_10DOF.h>
-
-Adafruit_10DOF                dof   = Adafruit_10DOF();
-Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
-Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
-Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
-float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;  /* Update this with the correct SLP for accurate altitude measurements */
-
-//SENSOR INITILIZATION
-void setup(void)
-{
-  Wire.begin();
-  Serial.begin(9600);
-  //initilize_HMC588L_MAGNETOMETER();
-  initilize_Adafruit_10_DOF_Sensors();
-}
-
-
-void loop (){
- float descent_angle, Heading, Altitude, Temperature, Roll;
- adafruit_function (&descent_angle, &Heading, &Altitude, &Temperature, &Roll);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//***********************************************************************************************************************************
-//***********************************************************************************************************************************
-//***********************************************************************************************************************************
-//***********************************************************************************************************************************
-//***********************************************************************************************************************************
-//ADD YOUR FUNCTIONS HERE--ADD YOUR FUNCTIONS HERE--ADD YOUR FUNCTIONS HERE
-/*--DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS--*/
-void  initilize_Adafruit_10_DOF_Sensors()
-{
-  if(!accel.begin())
-  {Serial.println("ERROR accelerometer"); while(1);}
-  if(!mag.begin())
-  {Serial.println("ERROR mahnetometer"); while(1);}
-  if(!bmp.begin())
-  { Serial.println("ERROR pressure");while(1);}
-}
-void adafruit_function (float *descent_angle, float *Heading, float *Altitude, float *Temperature, float *Roll)
-{ 
-  sensors_event_t accel_event;     sensors_event_t mag_event;
-  sensors_event_t bmp_event;       sensors_vec_t   orientation;
-     accel.getEvent(&accel_event);
-  if (dof.accelGetOrientation(&accel_event, &orientation))
-  {/* 'orientation' should have valid .roll and .pitch fields *///Serial.print(F("Roll: "));  Serial.print(orientation.roll);Serial.print(orientation.pitch);
-   *descent_angle = orientation.pitch;
-    *Roll = orientation.roll;}
-      mag.getEvent(&mag_event);
-  if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
-  {  *Heading = orientation.heading;}
-      bmp.getEvent(&bmp_event);
-  if (bmp_event.pressure)
-  {float Temp;
-   bmp.getTemperature(&Temp);
-   *Temperature = Temp;
-   *Altitude = bmp.pressureToAltitude(seaLevelPressure, bmp_event.pressure, Temp); 
+  //THIS CODE IS FOR THE ADAFRUIT 10 DOF SENSOR------
+  //CHECK BUTTOM FOR FUNCTIONS
+  #include <Wire.h> 
+  #include <Adafruit_Sensor.h>
+  #include <Adafruit_LSM303_U.h>              
+  #include <Adafruit_BMP085_U.h>
+  #include <Adafruit_L3GD20_U.h>
+  #include <Adafruit_10DOF.h>
+  
+  Adafruit_10DOF                dof   = Adafruit_10DOF();
+  Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+  Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+  Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
+  
+  Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20); //Gyro deceleration
+  
+  float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;  /* Update this with the correct SLP for accurate altitude measurements */
+  
+  //SENSOR INITILIZATION
+  void setup(void)
+  {
+    Wire.begin();
+    Serial.begin(9600);
+    
+    gyro.enableAutoRange(true);           //Enable auto range gyro
+    initilize_Adafruit_10_DOF_Sensors();  //Enable adafruit sensors;
+    
   }
-}
+  
+  
+    void loop (){
+     float y_alpha, x_alpha, z_alpha, z_rollrate, Altitude, Temperature;
+     adafruit_function (&y_alpha, &x_alpha, &z_alpha, &z_rollrate, &Altitude, &Temperature);
+     
+     //Serial.print (z_rollrate);
+     //Serial.print ("******************");
+     //Serial.print (z_alpha);
+     //Serial.print ("******************");
+     //Serial.print (x_alpha);
+     //Serial.print ("******************");
+     //Serial.print (y_alpha);
+     //Serial.println ("******************");
+     //delay (50);
+    }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //***********************************************************************************************************************************
+  //***********************************************************************************************************************************
+  //***********************************************************************************************************************************
+  //***********************************************************************************************************************************
+  //***********************************************************************************************************************************
+  //ADD YOUR FUNCTIONS HERE--ADD YOUR FUNCTIONS HERE--ADD YOUR FUNCTIONS HERE
+  /*--DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS----DO NOT CROSS--*/
+  void  initilize_Adafruit_10_DOF_Sensors()
+  {
+    if(!accel.begin())
+    {Serial.println("ERROR Accelerometer"); while(1);}
+    if(!mag.begin())
+    {Serial.println("ERROR Magnetometer"); while(1);}
+    if(!bmp.begin())
+    { Serial.println("ERROR Pressure");while(1);}
+    if(!gyro.begin())
+    { Serial.println("ERROR Gyroscope");while(1);}
+  }
+  void adafruit_function (float *y_alpha, float *x_alpha, float *z_alpha, float *z_rollrate, float *Altitude, float *Temperature)
+  { 
+    sensors_event_t accel_event;        sensors_event_t mag_event;       sensors_event_t gyro_event;          sensors_event_t bmp_event;   
+    sensors_vec_t   orientation;
+    accel.getEvent(&accel_event);                                        gyro.getEvent(&gyro_event);
+    
+    if (dof.accelGetOrientation(&accel_event, &orientation))
+    {/* 'orientation' should have valid .roll and .pitch fields *///Serial.print(F("Roll: "));  Serial.print(orientation.roll);Serial.print(orientation.pitch);
+     *y_alpha = orientation.pitch;
+      *x_alpha = orientation.roll;}
+        mag.getEvent(&mag_event);
+    if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
+    {  *z_alpha = orientation.heading;}
+        bmp.getEvent(&bmp_event);
+    if (bmp_event.pressure)
+    {float Temp;
+     bmp.getTemperature(&Temp);
+     *Temperature = Temp;
+     *Altitude = bmp.pressureToAltitude(seaLevelPressure, bmp_event.pressure, Temp); 
+     
+     //z_rollrate = gyro_event.gyro.z; //rad/s
+     *z_rollrate = (gyro_event.gyro.z) * (180/3.142); //deg/s
+     
+    }
+  }
 
